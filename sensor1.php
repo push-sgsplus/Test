fbc2d26722ff38dd8951df63c83c0533a26b34f2
Sven Jautz <tribual@web.de>
2012-10-18 11:58:31 +0200

    Test
    just for testing

---
+++ sensor1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="refresh" content="5"/>
+    <meta http-equiv="cache-control" content="no-cache">
+    <title>Sauerstoffs㳴igung und Temperatur der einzelnen Becken</title>
+</head>
+<body bgcolor="#ffffff" text="#000000" link="#804080" vlink="#603060" alink="#804080">
+<center>
+<b><u>Sensor Becken 1</u></b>
+<br>
+<br>
+<img src="becken.svg" width="100" alt="Becken Logo">
+<br>
+<br>
+<?PHP
+phpinfo ();
+?>
+
+</center>
+</body>
+</html>
